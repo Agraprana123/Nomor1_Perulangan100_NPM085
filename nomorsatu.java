bfce672fcc925332760022f973602650ca450096
Agraprana123 <147461035+Agraprana123@users.noreply.github.com>
2023-11-03 23:54:34 -0700

    Add files via upload

---
+++ nomorsatu.java
@@ -0,0 +1,13 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class nomorsatu{
+    public static void main(String[] args) {
+        for(int i=1; i<=100; i++){
+            if(i <= 9){
+                System.out.println(i);
+            } else {
+                System.out.println("Juan");
+            }
+        }
+        }
+    }
